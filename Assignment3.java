adce4970dbb63cf78d061505b82e460f37b29b4e
Rosemarie Prestoza Limos <25ur0597@psu.edu.ph>
2026-09-03 09:21:03 +0800

    add Assignment3.java

---
+++ Assignment3.java
@@ -0,0 +1,19 @@
+public class Assignment3 {//Main class
+    public static void main(String[] args) {// this is where the program starts
+
+        String firstName = "Rosemarie";//Stores my firstname as a string
+        String lastName = "Limos";//Stores my lastname as a string
+        String address = "Haway Pozorrubio Pangasinan";//Stores my address as a string
+        String mobileNumber = "09671855381";//Stores my mobile number as a string
+        int age = 19;//Stores my age as an integer
+        double gwa = 1.78;//Stores my GWA as a decimal number
+
+        System.out.println("First name: " + firstName);//Displays my firstname
+        System.out.println("Last name: " + lastName);//Displays my lastname
+        System.out.println("Address: " + address);//Displays my address
+        System.out.println("Mobile number: " + mobileNumber);//Displays my mobile number
+        System.out.println("Age: " + age);//Displays my age
+        System.out.println("GWA: " + gwa);//Displays my GWA
+
+    }//Ends the main method
+}//Ends the program
